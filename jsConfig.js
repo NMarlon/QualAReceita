1f6b09accbcf916a81634dc85934a6c791fc68e3
Marlon Rebello Viana <marlonrviana@gmail.com>
2019-03-27 20:27:27 -0300

    Mais Algumas adições no Plano Cartesiano

--- jsConfig.js
+++ jsConfig.js
@@ -36,40 +36,72 @@ function atualizarMiniMapa(){ //Iniciar com algumas receitas (Por enquanto apena
     var c = document.getElementById("myCanvas");//Canvas
     var ctx = c.getContext("2d");
 
+    ctx.clearRect(0, 0, c.width, c.height); //Resetar Canvas a Cada Atualização (!Para não haver sobreposição de bolinhas!)
+
     var bar1 = new ldBar("#loading"); //loading
     var bar2 = document.getElementById('loading').ldBar;
-   
 
-	ctx.clearRect(0, 0, c.width, c.height); //Resetar Canvas a Cada Atualização (!Para não haver sobreposição de bolinhas!)
+    var numDeBolinhas = document.getElementById('numDeBolinhasId').value;
 
-	var numDeBolinhas = document.getElementById('numDeBolinhasId').value;
-	//document.getElementById('receitasFeed').id = 'receitasFeed'+i;
-	//setTimeout(loopPOsicao Receitas, 2); //talvez precise
-	//ESTRUTURA PHI
-	var r = numDeBolinhas; 			//(pixels) r Inicial-----------------------------------------------r inicial
-	//const pi = Math.PI;//3.14159265358979323846264338327950288;
-	var angulo = 1;//2*pi; //Angulo de rotação
-	const initNumBolinhas = document.getElementById('initNumBolinhasId').value;
-	let iInit = 1;//Valor Inicial da contagem:
-	for (let i = iInit; i <= numDeBolinhas; i++) {
-		let j = i + parseInt(initNumBolinhas);
-		var proporcao = document.getElementById('razaoNumId').value;//o; //PHI; user vai poder ajustar mais tarde nas configurações -------- AINDA TEM QUE CRIAR PÁGINA DE CONFIGURAÇÕES
-		angulo = j * proporcao * 2 * Math.PI;
-		let x = r*Math.cos(angulo);//coordenadas polares para coordenadas cartesianas
-		let y = r*Math.sin(angulo);
-		ctx.beginPath();
-		ctx.arc(x+100,y+100,4.8,0,2*Math.PI);
-		ctx.stroke();
-		//document.getElementById('receitasFeed'+i).style.left = x; //x
-		//document.getElementById('receitasFeed'+i).style.bottom = y;//y
-		let rBolinhas = (45/50000)*document.getElementById('DistBolinhas').value; // 50000 é o raio padrão das RECEITAS, 45 é o padrão para esse canvas... Regra de 3, 50000 -> 45
-		r = Math.sqrt(j*rBolinhas); //j * A distância entre eles
-		bar2.set((i-iInit) / (numDeBolinhas-iInit));
+    let iInit = 1; //Valor Inicial da contagem:
+    if(document.getElementById("planoPolar").checked == true){//Se for Plano Polar
+		
+		//document.getElementById('receitasFeed').id = 'receitasFeed'+i;
+		//setTimeout(loopPOsicao Receitas, 2); //talvez precise
+		//ESTRUTURA PHI
+		var r = numDeBolinhas; 			//(pixels) r Inicial-----------------------------------------------r inicial
+		//const pi = Math.PI;//3.14159265358979323846264338327950288;
+		var angulo = 1;//2*pi; //Angulo de rotação
+		const initNumBolinhas = document.getElementById('initNumBolinhasId').value;
+
+		for (let i = iInit; i <= numDeBolinhas; i++) {
+			let j = i + parseInt(initNumBolinhas);
+			var proporcao = document.getElementById('razaoNumId').value;//o; //PHI; user vai poder ajustar mais tarde nas configurações -------- AINDA TEM QUE CRIAR PÁGINA DE CONFIGURAÇÕES
+			angulo = j * proporcao * 2 * Math.PI;
+			let x = r*Math.cos(angulo);//coordenadas polares para coordenadas cartesianas
+			let y = r*Math.sin(angulo);
+			ctx.beginPath();
+			ctx.arc(x+100,y+100,4.8,0,2*Math.PI);
+			ctx.stroke();
+			//document.getElementById('receitasFeed'+i).style.left = x; //x
+			//document.getElementById('receitasFeed'+i).style.bottom = y;//y
+			let rBolinhas = (45/50000)*document.getElementById('DistBolinhas').value; // 50000 é o raio padrão das RECEITAS, 45 é o padrão para esse canvas... Regra de 3, 50000 -> 45
+			r = Math.sqrt(j*rBolinhas); //j * A distância entre eles
+			bar2.set((i-iInit) / (numDeBolinhas-iInit));//Função da Porcentagem
+		}
+	}else{//Se for plano cartesiano
+		x=0;
+		y=0;
+		for (let i = iInit; i <= numDeBolinhas;i++) {
+			switch(i%4){
+				case 1:
+					x = i * document.getElementById('distanciaX').value;
+					distanciaCartesiana(ctx,x,y);
+				break;
+				case 2:
+					y = i * document.getElementById('distanciaY').value;
+					distanciaCartesiana(ctx,x,y);
+				break;
+				case 3:				
+					y = -i * document.getElementById('distanciaY').value;
+					distanciaCartesiana(ctx,x,y);
+				break;
+				default:
+					x = -i * document.getElementById('distanciaX').value;
+					distanciaCartesiana(ctx,x,y);
+			}
+		}
 	}
-
 }
 
-$(document).ready(function(){ //Ao mudar o tipo de padrão (#QualPadrao), muda o (#razaoNumId) 
+function distanciaCartesiana(ctx,x,y){
+	ctx.beginPath();
+	ctx.arc(x+100,y+100,4.8,0,2*Math.PI);//+100 é para centralizar com o canvas
+	ctx.stroke();
+}
+
+
+$(document).ready(function(){ //Ao mudar algum INPUT vai mudar todo o miniMapa, Sem precisar do botão ATUALIZAR
     $('#QualPadrao').change(function(){
     	switch(document.getElementById('QualPadrao').value) {
     		case '1':
@@ -104,11 +136,23 @@ $(document).ready(function(){ //Ao mudar o tipo de padrão (#QualPadrao), muda o
 		document.getElementById('razaoNumId').value = document.getElementById('razaoRangId').value;
 	   	atualizarMiniMapa();
 	});
+	//Plano CARTESIANO
+
+	$('input[name=planoName]').change( function() {
+   		atualizarMiniMapa();
+	});
+	$('#distanciaX').change(function(){
+		atualizarMiniMapa();
+	});
+	$('#distanciaY').change(function(){
+		atualizarMiniMapa();
+	})
+	
 });
 
 
 
-function resetMapa(){
+function resetMapa(){ // RESET DAS CONFIGURAÇÕES DA ESTRUTURA DAS RECEITAS
 	document.getElementById('QualPadrao').value = 1;
 	document.getElementById("razaoNumId").value = (1+Math.sqrt(5))/2; 
 	document.getElementById("razaoRangId").disabled = true;
@@ -117,7 +161,7 @@ function resetMapa(){
 	document.getElementById('DistBolinhas').value = 50000;
 	document.getElementById('numDeBolinhasId').value = 250;
 	document.getElementById('initNumBolinhasId').value = 1;
-
+	document.getElementById('planoPolar').checked = true;
 
 	atualizarMiniMapa();
 }
